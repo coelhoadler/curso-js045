9db3e2d0d8fe9760a620d62be3ec6eeab705951f
Adler Coelho <adler.coelho.santos@icloud.com>
2017-11-18 17:03:59 -0200

    Inserindo form.

--- caelumpic/src/app/cadastro/cadastro.component.ts
+++ caelumpic/src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FotoComponent } from 'app/Foto/foto.component';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,7 +11,9 @@ export class CadastroComponent implements OnInit {
 
   foto: FotoComponent;
 
-  constructor() {
+  constructor(
+    private _http: Http
+  ) {
     this.foto = new FotoComponent();
   }
 
@@ -18,7 +21,20 @@ export class CadastroComponent implements OnInit {
 
   cadastrar(event: Event) {
     event.preventDefault();
-    console.log(this.foto);
+    console.log('caindo aqui')
+    const cabecalho = new Headers();
+    cabecalho.append('Content-Type', 'application/json');
+
+    this._http.post(
+      'http://localhost:3000/v1/fotos',
+      JSON.stringify(this.foto),
+      { 
+        headers: cabecalho
+      }
+    ).subscribe(
+      res => console.log(res),
+      error => console.log(error)
+    )
   }
 
 }
